62fa942f0c74e3ec0758226081d3951d418f9f5b
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-10-18 11:57:07 -0700

    Little changes

--- portfolio-hisamy/src/Home.jsx
+++ portfolio-hisamy/src/Home.jsx
@@ -4,7 +4,7 @@ export function Home() {
         <div className="h-[calc(100vh-7rem)] bg-background">
             <div className="w-full h-full bg-red flex justify-between gap-2">
                 <div className="flex items-center justify-start">
-                    <div className="flex flex-col gap-8 ml-[70%]">
+                    <div className="flex flex-col gap-8 lg:ml-[70%]">
                         <div className="text-white text-center">
                             <p className="font-altone">Hola, I'm</p>
                             <p className="font-arsenica text-7xl">Hisamy</p>
@@ -21,9 +21,10 @@ export function Home() {
                     </div>
 
                 </div>
-                <div className="flex items-end h-full">
+                <div className="sm:flex sm:items-end h-full">
                     <img
-                        className="h-[90%] w-auto"
+                        className=" w-24 h-24 rounded-full border-4 border-background bg-background mb-4
+                        sm:w-auto sm:h-[90%] sm:rounded-none sm:border-0 sm:bg-transparent sm:mb-0"
                         src="https://i.pinimg.com/originals/a2/43/fe/a243feb03fbf40c21f18f63e28162700.png"
                         alt="Gato llorando"
                     />
